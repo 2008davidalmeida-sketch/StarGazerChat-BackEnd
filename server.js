cb817dd2dcb4b04e1f0ccd299c3d50c2f231a470
David Almeida <2008davidalmeida@gmail.com>
2026-04-10 20:10:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://stargazerchat.vercel.app'
+        origin: ['https://stargazerchat.vercel.app' , 'http://localhost:5173']
     }
 });
 
-app.use(cors({origin: 'https://stargazerchat.vercel.app'}));
+app.use(cors({origin: ['https://stargazerchat.vercel.app' , 'http://localhost:5173']}));
 app.use(express.json());
 app.use('/auth', authRoutes);  
 app.use('/messages', messageRoutes);  
